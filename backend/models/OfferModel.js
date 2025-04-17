ad0d54aa38353a8bd1e8d378dfa05bb027eedc06
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-04-17 11:15:31 -0300

    Creating a sequelize model: OfferModel

---
+++ backend/models/OfferModel.js
@@ -0,0 +1,33 @@
+
+const { DataTypes } = require('sequelize');
+const Connection = require('../connection/connection');
+
+const Offer = Connection.define('Offers', {
+    id: {
+        type: DataTypes.CHAR(36),
+        allowNull: false,
+        primaryKey: true
+    },
+    user_id: {
+        type: DataTypes.CHAR(36),
+        allowNull: false
+    },
+    request_id: {
+        type: DataTypes.CHAR(36),
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING(500),
+        allowNull: false
+    },
+    status: {
+        type: DataTypes.ENUM('pendente', 'aceito', 'rejeitado'),
+        allowNull: false,
+        defaultValue: 'pendente'
+    }
+    }, {
+        tableName: 'Offers',
+        timestamps: false
+    });
+  
+module.exports = Offer;
