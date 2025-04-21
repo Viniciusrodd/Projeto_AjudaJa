79b6309126ab07832ecdc1bc26526bcd3e2c255a
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-04-21 16:20:00 -0300

    Estructuring + style of Login page

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 
 // css
-import styles from './Login.module.css';
+import styles from '../Register/Register.module.css';
 
 // components
 import NavBar from '../../components/NavBar/NavBar';
@@ -9,7 +9,37 @@ const Login = () => {
     return (
         <div className='app_register_login'>
             <NavBar condition={ true } />
-            <h1>Login</h1>
+
+            <div className={ styles.register_container }>
+                <form className={ `align_default ${styles.register_fields_container}` }>
+                    <div className='img_container container_images'></div>
+
+                    <h1 className='title is-3 has-text-black'>Faça seu Login...</h1>
+
+                    <button className="button is-primary is-inverted">
+                        Continue com Google <img src="../../../images/google_icon.png"/>
+                    </button>
+
+                    <h1 className={ styles.subtitle }>Ou</h1>
+                    
+                    <hr className='hr' />
+
+                    <input type="text" name="name" className={ styles.input_register } 
+                    placeholder='Seu nome' />
+
+                    <input type="password" name="password" className={ styles.input_register } 
+                    placeholder='Sua senha' />
+
+                    <input type="password" name="confirm_password" className={ styles.input_register } 
+                    placeholder='Confirme sua senha' />
+
+                    <hr className='hr' />
+
+                    <button className="button is-primary is-inverted">
+                        Concluir
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
