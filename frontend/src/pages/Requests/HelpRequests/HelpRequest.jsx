e1b7f6825e960426efb100530106d658e7cda3e7
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-06-13 11:45:36 -0300

    Modal refactoring at help requests page

--- frontend/src/pages/Requests/HelpRequests/HelpRequest.jsx
+++ frontend/src/pages/Requests/HelpRequests/HelpRequest.jsx
@@ -17,22 +17,26 @@ import { postRequest } from '../../../services/RequestHelpServices';
 import { UserContext } from '../../../context/UserContext';
 
 const HelpRequest = () => {
-    // context
-    const { userId } = useContext(UserContext);
-
     // states
     const [ userFields, setUserFields ] = useState({
         title: '', description: '', category: 'livre', urgency: 'baixa', latitude: 0, longitude: 0
     });
     const [ redirect, setRedirect ] = useState(false);
+    
+    // modal
+    const [ modal_display, setModal_display ] = useState(false);
+    const [ modal_title, setModal_title ] = useState(null);
+    const [ modal_msg, setModal_msg ] = useState(null);
+    const [ modal_btt, setmodal_btt ] = useState(false);
+    const [ modal_btt_2, setModal_btt_2 ] = useState(false);
+    const [ title_color, setTitle_color ] = useState('#000');
+    
+    // consts
+    const { userId } = useContext(UserContext);
     const navigate = useNavigate();
+    
 
-    // refs
-    const modal = useRef(null);
-    const modal_title = useRef(null);
-    const modal_msg = useRef(null);
-    const modal_btt = useRef(null);
-    const modal_btt_2 = useRef(null);
+    ////////////// functions
 
 
     // redirect user to homepage
@@ -48,24 +52,46 @@ const HelpRequest = () => {
         }
     }, [redirect]);
 
+    // modal config
+    const modal_config = ({ title, msg, btt1, btt2, display, title_color }) => {
+        setModal_title(title ?? null);
+        setModal_msg(msg ?? null);
+        setmodal_btt(btt1 ?? false);
+        setModal_btt_2(btt2 ?? false);
+        setModal_display(display ?? false);
+        setTitle_color(title_color ?? '#000');
+
+        // The "??" (nullish coalescing operator) 
+        // returns the value on the right ONLY if the value on the left is null or undefined
+    };    
+    
+    // close modal
+    const closeModal = () =>{
+        if(modal_btt_2 !== null){
+            modal_config({
+                title: null, msg: null, btt1: false, 
+                btt2: false, display: false, title_color: '#000'
+            });
+        }
+    };
 
     // get user location + advice
     useEffect(() =>{
-        // modal message
-        modal.current.style.display = 'flex';
-        modal_title.current.innerText = 'Lembrete...'
-        modal_msg.current.innerText = `
-        Usamos sua localização para\n complementar seu pedido !!!\n
-        FIQUE TRANQUILO/A\n
-        isso torna a ajuda ainda mais fácil,\n uma vez que sabemos ONDE ajudar...`;
-        modal_btt.current.style.display = 'none';
-        modal_btt_2.current.style.display = 'none';
-
+        modal_config({
+            title: 'Espere',
+            msg: `
+            Usamos sua localização para\n complementar seu pedido !!!\n
+            FIQUE TRANQUILO/A\n
+            isso torna a ajuda ainda mais fácil,\n uma vez que sabemos ONDE ajudar...`,
+            btt1: false, btt2: false,
+            display: 'flex', title_color: 'rgb(0, 136, 255)'
+        });
         
         const clearModal = setTimeout(() =>{
-            modal_btt.current.style.display = 'flex';
-            modal_btt_2.current.style.display = 'flex';    
-            modal.current.style.display = 'none';
+            modal_config({
+                title1: null, msg: null, btt1: false, 
+                btt2: false, display: false, title_color: '#000'
+            });
 
             // get navigation location
             if(navigator.geolocation){
@@ -81,13 +107,12 @@ const HelpRequest = () => {
             }else{
                 console.warn("Geolocalização não suportada neste navegador.")
             }
-        }, 4000);
+        }, 6000);
 
         return () =>{
             clearTimeout(clearModal);
         };
     }, []);    
-
 
     // handle form
     const handleForm = async (e) =>{
@@ -97,60 +122,66 @@ const HelpRequest = () => {
             const response = await postRequest(userFields, userId);
 
             if(response.status === 200){
-                modal.current.style.display = 'flex';
-                modal_title.current.innerText = 'Sucesso!!!'
-                modal_msg.current.innerText = `Pedido de ajuda postado! \n 
-                você será redirecionado para a página principal...`;
-                modal_btt.current.style.display = 'none';
-                modal_btt_2.current.style.display = 'none';
-
+                modal_config({
+                    title: 'Sucesso',
+                    msg: `Pedido de ajuda postado! \n 
+                    você será redirecionado para a página principal...`,
+                    btt1: false, btt2: false,
+                    display: 'flex', title_color: 'rgb(38, 255, 0)'
+                });
                 setRedirect(true);
             }
         }
         catch(error){
             console.log('Error at create a help request: ', error);
 
-            modal.current.style.display = 'flex';
-            modal_title.current.innerText = 'Erro'
-            modal_msg.current.innerText = 'Erro ao criar pedido de ajuda...'
-
-            modal_btt.current.innerText = 'Tentar novamente'
-            modal_btt_2.current.style.display = 'none';
-
-            modal_btt.current.onclick = () => {
-                modal.current.style.display = 'none';
-            }; 
+            modal_config({
+                title: 'Erro',
+                msg: `Erro ao criar pedido de ajuda...`,
+                btt1: false, btt2: 'Tentar novamente',
+                display: 'flex', title_color: 'rgb(255, 0, 0)'
+            });
         }
-    }; 
+    };
+    
+    
+    ////////////// jsx
 
 
     return (
         <div className='forms_container'>
             { /* Modal */ }
-            <div className='modal' ref={ modal }>
+            <div className='modal' style={{ display: modal_display ? 'flex' : 'none' }}>
             <div className='modal-background'></div>
                 <div className='modal-card'>
                     <header className='modal-card-head'>
-                        <p className='modal-card-title' style={{ textAlign:'center' }} ref={ modal_title }>
-                            Espere um pouco
+                        <p className='modal_title modal-card-title has-text-centered' 
+                        style={{ textAlign:'center', color: title_color }}>
+                            { modal_title }
                         </p>
                     </header>
                     <section className='modal-card-body'>
-                        <p className='modal-card-title' ref={ modal_msg } style={{ textAlign:'center' }}>Mensagem de aviso...</p>
+                        <p className='modal-card-title has-text-centered' style={{ textAlign:'center' }}>
+                            { modal_msg }
+                        </p>
                     </section>
                     <footer className='modal-card-foot is-justify-content-center'>
                         <div className='div-buttons'>
-                            <button className="button is-danger is-dark" ref={ modal_btt }>
-                                Excluir
-                            </button>
-                            <button className="button is-primary is-dark" ref={ modal_btt_2 } style={{ marginLeft:'10px' }}>
-                                Voltar
-                            </button>
+                            {modal_btt && (
+                                <button className="button is-danger is-dark">
+                                    { modal_btt }
+                                </button>
+                            )}
+                            {modal_btt_2 && (
+                                <button onClick={ closeModal } className="button is-primary is-dark" 
+                                style={{ marginLeft:'10px' }}>
+                                    { modal_btt_2 }
+                                </button>
+                            )}
                         </div>
                     </footer>
                 </div>
-            </div>
-
+            </div>            
 
             { /* sidebar */ }
             <SideBar />
