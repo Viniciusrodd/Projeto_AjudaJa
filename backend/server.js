8e1820c3eb3f63ab829a55802191c6cdb9962f69
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-06-20 10:51:07 -0300

    Socket.io server config

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
 
 // modules
 const app = require('./app.js');
+const http = require('http');
+const { Server } = require('socket.io');
 const Connection = require('./Database/Connection/connection.js');
 const mongoConnection = require('./mongoDatabase/mongoConnection.js');
+const chatSocket = require('./services/sockets/chatSocket.js'); // chatSocket service
 require('dotenv').config();
 const relations = require('./Database/Relations.js');
 
+
+// socket io server
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: process.env.FRONTEND_PORT,
+        credentials: true
+    }
+});
+// init sockets
+chatSocket(io);
 
 
 Connection.authenticate()
