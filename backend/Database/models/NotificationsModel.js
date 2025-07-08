b7f58221f93669dc2823a62de4e21bd5850b75b4
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-07-08 15:11:37 -0300

    Notifications sequelize model

---
+++ backend/Database/models/NotificationsModel.js
@@ -0,0 +1,35 @@
+
+const { DataTypes } = require('sequelize');
+const Connection = require('../Connection/connection');
+const { v4: uuidv4 } = require('uuid');
+
+
+const Notification = Connection.define('Notifications', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4, // generating random uuid with lib 'uuid'
+        primaryKey: true
+    },
+    user_id: {
+        type: DataTypes.CHAR(36),
+        allowNull: false
+    },
+    from_user_id: {
+        type: DataTypes.CHAR(36),
+        allowNull: false
+    },
+    is_read: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    }
+    }, {
+        tableName: 'notifications',
+        timestamps: false
+});
+
+
+module.exports = Notification;
